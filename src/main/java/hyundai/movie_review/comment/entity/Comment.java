2cd00f2253996bc95b3dbcd03f6f19d56eb5cdec
Zoonmy <pjm2571@naver.com>
2024-11-08 15:23:13 +0900

    feat: Comment Entity 생성

---
+++ src/main/java/hyundai/movie_review/comment/entity/Comment.java
@@ -0,0 +1,33 @@
+package hyundai.movie_review.comment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "COMMENT")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "member_id")
+    private Long memberId;
+    @Column(name = "review_id")
+    private Long reviewId;
+    private String content;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
