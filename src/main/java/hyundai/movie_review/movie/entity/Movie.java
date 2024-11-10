0a294d5d5a175d204df5983a9243f851ec1ef6e9
Zoonmy <pjm2571@naver.com>
2024-11-10 17:18:22 +0900

    feat: Movie Entity 구현 #9

---
+++ src/main/java/hyundai/movie_review/movie/entity/Movie.java
@@ -0,0 +1,55 @@
+package hyundai.movie_review.movie.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MOVIE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String overview;
+
+    @Column(name = "poster_path")
+    private String posterPath;
+
+    @Column(name = "backdrop_path")
+    private String backdropPath;
+
+    private Boolean adult;
+
+    @Column(name = "release_date")
+    private LocalDateTime releaseDate;
+
+    private int runtime;
+
+    @Column(name = "origin_country")
+    private String originCountry;
+
+    @Column(name = "total_review_count")
+    private Long totalReviewCount;
+
+    @Column(name = "total_star_rate")
+    private Double totalStarRate;
+
+
+}
