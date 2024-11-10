8ebbf33fc64e1191f38402f61d90a480b52ba502
Zoonmy <pjm2571@naver.com>
2024-11-10 17:57:36 +0900

    refactgor: movie ddl 수정 - movie entity 정보 반영

--- ddl/movie.sql
+++ ddl/movie.sql
@@ -1,12 +1,15 @@
 CREATE TABLE MOVIE
 (
-    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
-    movie_id       BIGINT,
-    title          VARCHAR(255) NOT NULL,
-    overview       TEXT,
-    poster_path    VARCHAR(255),
-    adult          BOOLEAN,
-    release_date   DATE,
-    runtime        INT,
-    origin_country VARCHAR(255)
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
+    movie_id           BIGINT,
+    title              VARCHAR(255) NOT NULL,
+    overview           TEXT,
+    poster_path        VARCHAR(255),
+    backdrop_path      VARCHAR(255),
+    adult              BOOLEAN,
+    release_date       DATE,
+    runtime            INT,
+    origin_country     VARCHAR(255),
+    total_review_count BIGINT DEFAULT 0,
+    total_star_rate    DOUBLE DEFAULT 0.0
 );
