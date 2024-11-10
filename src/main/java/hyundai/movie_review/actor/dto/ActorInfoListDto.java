1e780ee060c5d75bd9c18435b3e651689b21763d
Zoonmy <pjm2571@naver.com>
2024-11-10 22:01:23 +0900

    feat: Movie 상세 정보를 가져오기 위한 dto 생성 #1

---
+++ src/main/java/hyundai/movie_review/actor/dto/ActorInfoListDto.java
@@ -0,0 +1,12 @@
+package hyundai.movie_review.actor.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+@Schema(description = "영화 배우의 리스트 DTO")
+public record ActorInfoListDto(
+        @Schema(description = "영화에 출연하는 배우들의 전체 리스트")
+        List<ActorInfoDto> actorList
+) {
+
+}
