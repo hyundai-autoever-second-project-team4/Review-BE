3c6916bb79272c8893683ad566686f5ea1211383
Zoonmy <pjm2571@naver.com>
2024-11-12 15:10:25 +0900

    feat: movie controller spring doc 추가

--- src/main/java/hyundai/movie_review/movie/controller/MovieController.java
+++ src/main/java/hyundai/movie_review/movie/controller/MovieController.java
@@ -1,8 +1,18 @@
 package hyundai.movie_review.movie.controller;
 
+import hyundai.movie_review.exception.BusinessExceptionResponse;
 import hyundai.movie_review.movie.dto.MovieDetailResponse;
 import hyundai.movie_review.movie.entity.Movie;
 import hyundai.movie_review.movie.service.MovieService;
+import hyundai.movie_review.review.dto.ReviewCreateResponse;
+import hyundai.movie_review.review.dto.ReviewDeleteResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Movie", description = "영화 관련 API")
 public class MovieController {
 
     private final MovieService movieService;
 
+    @Operation(summary = "영화 상세 조회", description = "특정 영화의 상세 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "영화 조회 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MovieDetailResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content())
+    })
     @GetMapping("/movie/{movieId}")
     public ResponseEntity<?> getMovieDetail(
-            @PathVariable Long movieId
+            @Parameter(description = "영화 ID", required = true) @PathVariable Long movieId
     ) {
         MovieDetailResponse response = movieService.getMovieDetail(movieId);
-
         return ResponseEntity.ok(response);
     }
 }
