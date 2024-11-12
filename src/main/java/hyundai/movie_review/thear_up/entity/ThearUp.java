0bc2ae99d55852f36ece1cb4a4ec22549ee9fcbb
yeon-dong <jae073@naver.com>
2024-11-12 09:52:23 +0900

    thearup table 생성 코드

---
+++ src/main/java/hyundai/movie_review/thear_up/entity/ThearUp.java
@@ -0,0 +1,22 @@
+package hyundai.movie_review.thear_up.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "THEAR_UP")
+public class ThearUp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    public ThearUp(Long memberId, Long reviewId){
+        this.memberId = memberId;
+        this.reviewId = reviewId;
+    }
+}
