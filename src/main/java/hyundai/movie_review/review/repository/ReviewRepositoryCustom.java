0eeb88a1f468b107e804338fb746efa326b37983
LYJ22 <pippi478@gmail.com>
2024-11-13 17:44:08 +0900

    feat: 영화 상세보기 리뷰정보 전달형식 리팩토링

--- src/main/java/hyundai/movie_review/review/repository/ReviewRepositoryCustom.java
+++ src/main/java/hyundai/movie_review/review/repository/ReviewRepositoryCustom.java
@@ -1,9 +1,6 @@
 package hyundai.movie_review.review.repository;
 
-import hyundai.movie_review.review.dto.ReviewInfoListDto;
 import hyundai.movie_review.review.dto.ReviewCountListDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface ReviewRepositoryCustom {
 
