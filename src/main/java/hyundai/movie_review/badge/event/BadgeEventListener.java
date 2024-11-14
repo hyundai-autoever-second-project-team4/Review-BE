da61b0e241f00f3a55c2e9f52e72ba4dd4a614cd
Zoonmy <pjm2571@naver.com>
2024-11-14 16:59:44 +0900

    feat: 작성한 리뷰에 해당하는 영화 장르 카운트 별 뱃지 지급 기능 구현

--- src/main/java/hyundai/movie_review/badge/event/BadgeEventListener.java
+++ src/main/java/hyundai/movie_review/badge/event/BadgeEventListener.java
@@ -1,15 +1,22 @@
 package hyundai.movie_review.badge.event;
 
 import hyundai.movie_review.badge.constant.BadgeConditionByBadgeCount;
+import hyundai.movie_review.badge.constant.BadgeConditionByGenreCount;
 import hyundai.movie_review.badge.constant.BadgeConditionByTotalReviewCount;
 import hyundai.movie_review.badge.entity.Badge;
 import hyundai.movie_review.badge.exception.BadgeIdNotFoundException;
 import hyundai.movie_review.badge.repository.BadgeRepository;
+import hyundai.movie_review.genre.entity.Genre;
 import hyundai.movie_review.member.entity.Member;
 import hyundai.movie_review.member.repository.MemberRepository;
 import hyundai.movie_review.member_badge.entity.MemberBadge;
 import hyundai.movie_review.member_badge.repository.MemberBadgeRepository;
+import hyundai.movie_review.movie.vo.MovieGenreCountMap;
+import hyundai.movie_review.movie_genre.entity.MovieGenre;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -32,7 +39,28 @@ public class BadgeEventListener {
 
         handleBadgeAwardByReviewCount(member);  // 총 리뷰 카운트에 대한 뱃지 어워드
         handleBadgeAwardByBadgeCount(member);   // 총 뱃지 카운트에 대한 뱃지 어워드
+        handleBadgeAwardByGenreCount(member);   // 작성한 리뷰 기반 장르 카운트에 대한 뱃지 어워드
 
+    }
+
+    private void handleBadgeAwardByGenreCount(Member member) {
+        // 1) member의 리뷰 목록에서 각 장르의 리뷰 개수를 카운트
+        Map<String, Long> genreCountMap = member.getReviews().stream()
+                .filter(review -> !review.getDeleted()) // 삭제되지 않은 리뷰만 카운트
+                .flatMap(review -> review.getMovie().getGenres().stream()) // 각 리뷰의 MovieGenre 참조
+                .map(MovieGenre::getGenre) // MovieGenre에서 Genre를 추출
+                .collect(Collectors.groupingBy(Genre::getName, Collectors.counting()));
+
+        // 2) 각 장르별로 배지 조건을 검증하고 충족하는 모든 배지를 수여
+        genreCountMap.forEach((genreName, count) -> {
+            List<BadgeConditionByGenreCount> conditions = BadgeConditionByGenreCount.getBadgesByGenreAction(
+                    genreName, count);
+
+            conditions.forEach(condition -> {
+                log.info("[SUCCESS] 장르별 배지 기준 충족: {}, 장르 : {}, 카운트 : {}", condition.getDescription(), condition.getGenre(), condition.getThreshold());
+                awardBadgeIfNotReceived(member, condition.getBadgeId());
+            });
+        });
     }
 
     private void handleBadgeAwardByBadgeCount(Member member) {
