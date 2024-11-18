4f56169b5228f3254261d04292075bbc5cd1cb56
Zoonmy <pjm2571@naver.com>
2024-11-18 12:29:15 +0900

    faet: mypage에서 주는 알람 정보 수정

--- src/main/java/hyundai/movie_review/alarm/entity/Alarm.java
+++ src/main/java/hyundai/movie_review/alarm/entity/Alarm.java
@@ -14,12 +14,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
+@Where(clause = "is_read = false")
 public class Alarm {
 
     @Id
