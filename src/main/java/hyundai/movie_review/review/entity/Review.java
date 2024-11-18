5eedb56b3cc02804635f8927153b262252b4fca3
Zoonmy <pjm2571@naver.com>
2024-11-18 09:21:41 +0900

    refactor: review 엔티티 조회 시, deleted false인 값만 가져오도록 수정

--- src/main/java/hyundai/movie_review/review/entity/Review.java
+++ src/main/java/hyundai/movie_review/review/entity/Review.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "REVIEW")
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Getter
+@Where(clause = "deleted = false")
 public class Review {
 
     @Id
